75725050f9f7f91be1d488f54275942437395a91
rolemadelen <rolemadelen17@pm.me>
2020-05-29 20:19:36 +0900

    normal keys are clikable. FnKeys and LogicalKeys are still in progress.

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "use strict"
 
 let running = true;
@@ -82,3 +83,52 @@ function play() {
 }
 
 play();
+=======
+'use strict';
+
+const normalKeys = document.querySelectorAll('#normal-keys button');
+const fnKeys = document.querySelectorAll('#fn-keys button');
+const displayArea = document.getElementById('display');
+let expr = '';
+let numberEntered = false;
+
+normalKeys.forEach(key => {
+	key.addEventListener('click', () => {
+
+		if(key.value === '=') {
+			if(expr === '') return;
+			let answer = Function('"use strict";return ('+expr+')')();
+			displayArea.children[0].value = answer;
+			expr = answer;
+		} else {
+			let t = displayArea.children[0];
+
+			if (Number(key.value) >= 0 && Number(key.value) <= 9) {
+				if(numberEntered == false || t.value==='0'){
+					t.value = key.value
+					numberEntered = true;
+				} else {
+					t.value += key.value
+				}
+			} else {
+				numberEntered = false;
+			}
+			expr += key.value;
+		}
+	});
+});
+
+fnKeys.forEach(key => {
+	key.addEventListener('click', () => {
+		if(key.value === "C") {
+			let t = displayArea.children[0];
+			t.value = '0';
+		} else if (key.value === "AC") {
+			displayArea.children[0].value = 0;
+			expr = ''
+		} else {
+
+		}
+	});
+});
+>>>>>>> eb5a49c (normal keys are clikable. FnKeys and LogicalKeys are still in progress.)
