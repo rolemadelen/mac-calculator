8219e9dcbd91d8c093a677713b654fabf7dd7d15
Madelen Yoo <rolemadelen@tuta.io>
2020-09-22 19:35:28 +0900

    Add arithmetic calculation using JS
    Used `eval()` to calculate numbers but will come up with other ways
    to computer an expression.
    
    When numbers are too long, it cuts off. Need to figure a way to
    change the font size based on the length of its number.
    
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

---
+++ 03_calculator/simple/js/main.js
@@ -0,0 +1,53 @@
+function operate(op) {
+    if (num2==='0') num2 = '';
+    let result = num2;
+
+    switch(op) {
+        case 'clear':
+            result = '0';
+            break;
+        case 'sign':
+            break;
+        case 'equal':
+            let temp = eval(num1 + " " + operation + " " + num2);
+            num1 = '';
+            num2 = '';
+            operation = null;
+            return temp;
+        case '.':
+            break;
+        case '%':
+        case '+': 
+        case '-':
+        case '*':
+        case '/':
+            num1 = num2;
+            num2 = '';
+            operation = op;
+            return '';
+        default:
+            result += op;
+    }
+
+    return result;
+}
+
+let num1 = '';
+let num2 = '';
+let operation = null;
+
+const resultPanel = document.querySelector('#result-panel #expr');
+
+let buttons = document.getElementsByTagName('button');
+for(let i=0; i<buttons.length; ++i) {
+    let button = buttons[i];
+    button.onclick =  function()  {
+        num2 = operate(button.value);
+        if (num2==='') {
+            resultPanel.textContent = num1;
+            num2 = '';
+        } else {
+            resultPanel.textContent = num2;
+        }
+    };
+}
