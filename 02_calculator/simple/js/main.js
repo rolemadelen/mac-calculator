d57da16a2eceece43af71d34b9f6787c94fe75c5
rolemadelen <rolemadelen17@pm.me>
2020-09-26 14:52:00 +0900

    Add single number calculation
    When pressed an operator with only one operands selected,
         it will perform calculation with that same number.

--- 02_calculator/simple/js/main.js
+++ 02_calculator/simple/js/main.js
@@ -1,20 +1,45 @@
 function operate(val) {
     let temp = result;
 
+    // clear calculation
     if (val === 'C') {
-        num = result = '0';
-        operation = '';
+        result = '0';
+        op = '';
         return result;
     }
+
    if (temp === '0') temp = val;
-   else temp += val;
+   else {
+       switch(val) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                op = val;
+                opSelected = true;
+                operand1 = temp;
+                return '';
+            case '=':
+                if(operand1 != null && operand2 === null) {
+                    if(result==='') result = operand1;
+                    let expr = result + op + operand1;
+                    temp = Function('"use strict"; return (' + expr + ')')();
+                } 
+                break;
+            default:
+                temp += val;
+       }
+   }
 
    return temp;
 }
 
-let num = '0';
-let operation = '';
+let op = '';
+let opSelected = false;
 let result = '0';
+let operand1 = null;
+let operand2 = null;
+
 
 const resultPanel = document.querySelector('#result-panel #expr');
 
@@ -24,6 +49,11 @@ for(let i=0; i<buttons.length; ++i) {
 
     button.onclick =  function()  {
         result = operate(button.value);
-        resultPanel.textContent = result;
+        if (result != '') resultPanel.textContent = result;
+        console.log("result: " + result);
+        console.log("operand1: " + operand1);
+        console.log("op:   " + op);
+        console.log("operand2: " + operand2);
+        console.log("---------------");
     };
 }
