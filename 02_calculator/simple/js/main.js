a365256c813b115884e3ce7498b7a156856594d2
rolemadelen <rolemadelen17@pm.me>
2020-09-29 20:40:50 +0900

    Add backspace and fixed '0' calculation

--- 02_calculator/simple/js/main.js
+++ 02_calculator/simple/js/main.js
@@ -1,3 +1,18 @@
+function calculate() {
+    while(operand1.length > 1 && operand1[0] === '0') {
+        operand1 = operand1.slice(1);
+        console.log("HELLLLLLLL: " + operand1);
+    }
+
+    while(operand2.length > 1 && operand2[0] === '0') {
+        console.log(operand2);
+        operand2 = operand2.slice(1);
+    }
+
+    let expr = operand1 + op + operand2;
+    return (Function('"use strict"; return (' + expr + ')')())+'';
+}
+
 function operate(val) {
     let temp = result;
 
@@ -14,55 +29,50 @@ function operate(val) {
         return '';
     }
 
-   if (temp === '0') return operand1;
-   else {
-       switch(val) {
-           case 'sign':
-               temp *= -1;
-               break;
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '%':
-                reset = false;
-                if(operand2 != '') {
-                    let expr = operand1 + op + operand2;
-                    operand1 = Function('"use strict"; return (' + expr + ')')();
-                    operand2 = '';
-                } else {
-                    operand1 = result;
-                }
-                op = val;
-                break;
-            case '=':
-                if(op==='') break;
-                if(operand2 === '') operand2 = saved;
-                else if(operand2 === temp*-1) operand2 = temp;
-                let expr = operand1 + op + operand2;
-                temp = Function('"use strict"; return (' + expr + ')')();
-                operand1 = temp;
+    switch(val) {
+        case 'sign':
+            temp *= -1;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+            reset = false;
+            if(operand2 != '') {
+                operand1 = calculate();
                 operand2 = '';
-            
-                reset = true;
-                break;
-            default:
-                // temp += val;
-                if(op === '') {
-                    operand1 += val;
-                    temp = operand1;
-                    reset = false;
-                }  else if (reset) {
-                    operand1 = val;
-                    temp = operand1;
-                    op = '';
-                } else { 
-                    operand2 += val;
-                    temp = operand2;
-                    saved = operand2;
-                }
-       }
-   }
+            } else {
+                operand1 = result;
+            }
+            op = val;
+            break;
+        case '=':
+            if(op==='') break;
+            if(operand2 === '') operand2 = saved;
+            else if(operand2 === temp*-1) operand2 = temp;
+            temp = calculate();
+            operand1 = temp;
+            operand2 = '';
+        
+            reset = true;
+            break;
+        default:
+            // temp += val;
+            if(op === '') {
+                operand1 += val;
+                temp = operand1;
+                reset = false;
+            }  else if (reset) {
+                operand1 = val;
+                temp = operand1;
+                op = '';
+            } else { 
+                operand2 += val;
+                temp = operand2;
+                saved = operand2;
+            }
+    }
 
    return temp;
 }
@@ -97,6 +107,16 @@ document.querySelector('body').addEventListener('keydown', (e) => {
         case 'sign':
             result = operate(e.key);
             break;
+        case 'Backspace':
+            if(result.length === 1) {
+                result = '0';
+            } else {
+                result = result.slice(0, -1);
+            }
+            if(operand2 != '') operand2 = result;
+            else operand1 = result;
+            resultPanel.textContent = result;
+            break;
         case 'Enter':
             result = operate('=');
             break;
@@ -112,6 +132,14 @@ document.querySelector('body').addEventListener('keydown', (e) => {
     } else if((result+'').length > 0) {
         resultPanel.style.fontSize = '45px';
     }
+
+    console.log("result: " + result);
+        console.log("operand1: " + operand1);
+        console.log("op:   " + op);
+        console.log("operand2: " + operand2);
+        console.log("saved: " + saved);
+        console.log("size: " + (result+'').length + ' (' + result +')');
+        console.log("---------------");
 })
 
 let buttons = document.querySelectorAll('#button-panel button');
